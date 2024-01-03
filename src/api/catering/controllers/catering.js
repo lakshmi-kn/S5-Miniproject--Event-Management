746dfde8759a5ba7cd8563640dfd8be5705b7e17
Lakshmi Narayanan <lakshmikn.2328@gmail.com>
2024-01-03 20:56:36 +0530

    Strapi backend

---
+++ src/api/catering/controllers/catering.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * catering controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::catering.catering');
